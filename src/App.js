f99dffb4561b3fcf3d1e4643435ba425fea29f14
Rohan Sriram <rohansriram96@gmail.com>
2021-03-05 07:16:23 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 import { data } from "./data";
 import NavBar from "./component/NavBar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
 import About from "./component/About";
 import Work from "./component/Work";
 import Projects from "./component/Projects";
@@ -10,23 +11,26 @@ import Contact from "./component/Contact";
 import Fotter from "./component/Footer";
 import Skills from "./component/Skills";
 
+
 const App = () => {
 	console.log(data.content);
 	return (
 		<div>
-			<Router>
+			<BrowserRouter basename ='/'>
 				<NavBar />
 				<Switch>
+				{/* <Link to="/About" /> */}
+					
 				    <Route exact path='/' component={About} />
 					<Route exact path='/Portfolio' component={About} />
 					<Route exact path='/About' component={About} />
 					<Route exact path='/Work' component={Work} />
-					 <Route exact path='/Skills' component={Skills} />
+					<Route exact path='/Skills' component={Skills} />
 					<Route exact path='/Projects' component={Projects} />
 					<Route exact path='/Contact' component={Contact} />
 				</Switch>
 				<Fotter />
-			</Router>
+			</BrowserRouter>
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
